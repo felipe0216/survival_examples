01065cbfb0ae5b9d671baf80aadad4cafcd81ad6
felipe0216 <felipe.araya.1993@gmail.com>
2021-04-28 01:29:38 +0100

    Create calculate_expectation.py

---
+++ calculate_expectation.py
@@ -0,0 +1,41 @@
+    
+    
+    penaliser=0.01
+    l1_ratio=0.99
+    for mdls in [WeibullAFTFitter(fit_intercept =True,penalizer=penaliser,l1_ratio=l1_ratio),LogNormalAFTFitter(fit_intercept=True,penalizer=penaliser,l1_ratio=l1_ratio), LogLogisticAFTFitter(penalizer =penaliser),CoxPHFitter(penalizer=penaliser,l1_ratio=l1_ratio)]:#,GeneralizedGammaRegressionFitter(penalizer=penaliser,l1_ratio=l1_ratio)]:
+        print(mdls)
+     
+        data__ = data_#data_[data_["target"]==1]
+        timelimes = sorted(data__[t_var].unique())
+        mdl = estimator
+        mdl.fit(data__,t_var, y_var)#,timeline =timelimes)
+        ci = mdl.score(data__,scoring_method="concordance_index")
+        print(ci)
+
+        # Pick only censored data
+        surv_data = data_[data_[y_var]==0]
+        t_var_ = t_var
+        target = y_var
+        surv_data_last_obs = surv_data[t_var_]
+
+
+
+        predict_expectation = surv_data.loc[:,[target,t_var_]]
+        try:
+            predict_expectation.loc[:,"expected"] = mdl.predict_expectation(surv_data)
+        except:
+            predict_expectation.loc[:,"expected"] = mdl.predict_expectation(surv_data,conditional_after =surv_data_last_obs )
+
+
+        predict_expectation.loc[:,"diff_expectation"] = predict_expectation.loc[:,"expected"] - predict_expectation.loc[:,t_var_] 
+
+        predict_median = mdl.predict_median(surv_data, conditional_after =surv_data_last_obs)
+        predict_expectation.loc[:,"median"] = predict_median
+        predict_expectation.loc[:,"diff_median"] = predict_expectation.loc[:,"median"] - predict_expectation[t_var_] 
+
+        mean_diff_median_method = predict_expectation.loc[predict_expectation["diff_median"]!=np.inf,"diff_median"].mean()
+        mean_diff_predict_expectation = predict_expectation["diff_expectation"].mean()
+        print(f"{np.round(mean_diff_predict_expectation,3)}")
+        print(f"{np.round(mean_diff_median_method,3)}")
+        print("\n")
+
