3919347db9181b34c1dae128c0249ddabb93b324
felipe0216 <felipe.araya.1993@gmail.com>
2021-04-28 01:44:48 +0100

    Update calculate_expectation.py

--- calculate_expectation.py
+++ calculate_expectation.py
@@ -1,19 +1,21 @@
 from lifelines import CoxPHFitter,GeneralizedGammaRegressionFitter,WeibullAFTFitter, LogNormalAFTFitter, LogLogisticAFTFitter
 import numpy as np
 import pandas as pd
-from lifelines.datasets import load_rossi
+from lifelines.datasets import load_leukemia
 
-t_var = "week"
-target = "arrest"
-data_ = load_rossi()
-penaliser=0.01
-l1_ratio=0.99
+t_var = "t"
+y_var = "status"
+data_ = load_leukemia()
+penaliser=0.00
+l1_ratio=0.00
+
+# There is a lifelines function which can help you decide which parametric function to use, but this is just for demonstration purposes.
 for mdls in [WeibullAFTFitter(fit_intercept =True,penalizer=penaliser,l1_ratio=l1_ratio),LogNormalAFTFitter(fit_intercept=True,penalizer=penaliser,l1_ratio=l1_ratio), LogLogisticAFTFitter(penalizer =penaliser),CoxPHFitter(penalizer=penaliser,l1_ratio=l1_ratio)]:#,GeneralizedGammaRegressionFitter(penalizer=penaliser,l1_ratio=l1_ratio)]:
     print(mdls)
 
     data__ = data_#data_[data_["arrest"]==1]
     timelimes = sorted(data__[t_var].unique())
-    mdl = estimator
+    mdl = mdls
     mdl.fit(data__,t_var, y_var)#,timeline =timelimes)
     ci = mdl.score(data__,scoring_method="concordance_index")
     print(ci)
@@ -24,17 +26,16 @@ for mdls in [WeibullAFTFitter(fit_intercept =True,penalizer=penaliser,l1_ratio=l
     target = y_var
     surv_data_last_obs = surv_data[t_var_]
 
-
-
+ 
+    # Using predict_expectation
     predict_expectation = surv_data.loc[:,[target,t_var_]]
     try:
         predict_expectation.loc[:,"expected"] = mdl.predict_expectation(surv_data)
     except:
         predict_expectation.loc[:,"expected"] = mdl.predict_expectation(surv_data,conditional_after =surv_data_last_obs )
-
-
     predict_expectation.loc[:,"diff_expectation"] = predict_expectation.loc[:,"expected"] - predict_expectation.loc[:,t_var_] 
 
+    # Using the median
     predict_median = mdl.predict_median(surv_data, conditional_after =surv_data_last_obs)
     predict_expectation.loc[:,"median"] = predict_median
     predict_expectation.loc[:,"diff_median"] = predict_expectation.loc[:,"median"] - predict_expectation[t_var_] 
